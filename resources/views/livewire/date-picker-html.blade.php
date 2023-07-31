633cfa5bc2b4e44c51dfc59b2f634de70a1507dd
Abdulrahman Mahidwei <a.mahidwei@outlook.com>
2023-07-31 17:03:21 +0200

    fix css

---
+++ resources/views/livewire/date-picker-html.blade.php
@@ -0,0 +1,69 @@
+<div class="litepicker" data-plugins="" style="display: inline-block; position: relative;">
+    <div class="container__main">
+        <div class="container__months">
+            <div class="month-item">
+                <div class="month-item-header">
+                  <button type="button" class="button-previous-month"><!-- Download SVG icon from http://tabler-icons.io/i/chevron-left -->
+                    @lang('Previous month')</button>
+                    <div><strong class="month-item-name">July</strong><span class="month-item-year">2023</span></div>
+                    <button type="button"
+                        class="button-next-month"><!-- Download SVG icon from http://tabler-icons.io/i/chevron-right -->
+                        <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24"
+                            viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none"
+                            stroke-linecap="round" stroke-linejoin="round">
+                            <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                            <path d="M9 6l6 6l-6 6"></path>
+                        </svg></button>
+                </div>
+                <div class="month-item-weekdays-row">
+                    <div title="Monday">Mon</div>
+                    <div title="Tuesday">Tue</div>
+                    <div title="Wednesday">Wed</div>
+                    <div title="Thursday">Thu</div>
+                    <div title="Friday">Fri</div>
+                    <div title="Saturday">Sat</div>
+                    <div title="Sunday">Sun</div>
+                </div>
+                <div class="container__days">
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div class="day-item" data-time="1688162400000" tabindex="0">1</div>
+                    <div class="day-item" data-time="1688248800000" tabindex="0">2</div>
+                    <div class="day-item" data-time="1688335200000" tabindex="0">3</div>
+                    <div class="day-item" data-time="1688421600000" tabindex="0">4</div>
+                    <div class="day-item" data-time="1688508000000" tabindex="0">5</div>
+                    <div class="day-item" data-time="1688594400000" tabindex="0">6</div>
+                    <div class="day-item" data-time="1688680800000" tabindex="0">7</div>
+                    <div class="day-item" data-time="1688767200000" tabindex="0">8</div>
+                    <div class="day-item" data-time="1688853600000" tabindex="0">9</div>
+                    <div class="day-item" data-time="1688940000000" tabindex="0">10</div>
+                    <div class="day-item" data-time="1689026400000" tabindex="0">11</div>
+                    <div class="day-item" data-time="1689112800000" tabindex="0">12</div>
+                    <div class="day-item" data-time="1689199200000" tabindex="0">13</div>
+                    <div class="day-item" data-time="1689285600000" tabindex="0">14</div>
+                    <div class="day-item" data-time="1689372000000" tabindex="0">15</div>
+                    <div class="day-item" data-time="1689458400000" tabindex="0">16</div>
+                    <div class="day-item" data-time="1689544800000" tabindex="0">17</div>
+                    <div class="day-item" data-time="1689631200000" tabindex="0">18</div>
+                    <div class="day-item" data-time="1689717600000" tabindex="0">19</div>
+                    <div class="day-item" data-time="1689804000000" tabindex="0">20</div>
+                    <div class="day-item" data-time="1689890400000" tabindex="0">21</div>
+                    <div class="day-item" data-time="1689976800000" tabindex="0">22</div>
+                    <div class="day-item" data-time="1690063200000" tabindex="0">23</div>
+                    <div class="day-item" data-time="1690149600000" tabindex="0">24</div>
+                    <div class="day-item" data-time="1690236000000" tabindex="0">25</div>
+                    <div class="day-item" data-time="1690322400000" tabindex="0">26</div>
+                    <div class="day-item" data-time="1690408800000" tabindex="0">27</div>
+                    <div class="day-item" data-time="1690495200000" tabindex="0">28</div>
+                    <div class="day-item" data-time="1690581600000" tabindex="0">29</div>
+                    <div class="day-item" data-time="1690668000000" tabindex="0">30</div>
+                    <div class="day-item is-today" data-time="1690754400000" tabindex="0">31</div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="container__tooltip"></div>
+</div>
--- resources/views/livewire/date-picker.blade.php
+++ resources/views/livewire/date-picker.blade.php
@@ -1,63 +1,60 @@
-<div class="{{ $this->wrapperClasses() }}">
-  <div class="litepicker">
+<div class="litepicker {{ $this->wrapperClasses() }}" style="display: inline-block; position: relative;">
     <div class="container__main">
-      <div class="container__months">
-        <div class="month-item">
-          <div class="month-item-header">
-            <button type="button" class="button-previous-month" wire:click="goPreviousMonth">
-              @lang('Previous month')
-            </button>
-            <div>
-              <strong class="month-item-name">{{ $monthName }}</strong>
-              <span class="month-item-year">{{ $year }}</span>
-            </div>
-            <button type="button" class="button-next-month" wire:click="goNextMonth">
-              @lang('Next month')</span>
-            </button>
-          </div>
-          <div class="month-item-weekdays-row">
-            @foreach ($weekDays as $index => $name)
-            <div title="{{ $name }}">
-              {{ \Illuminate\Support\Str::substr($name, 0, 1) }}
-            </div>
-            @endforeach
-          </div>
-          <div class="container__days">
-            @foreach ($dates as $index => $day)
-              @php
-                /** @var \Haringsrob\LivewireDatepicker\Dto\DatePickerDayData $dayData */
-                $dayData = $this->getAvailabilityFor($day);
-              @endphp
-              @if ($loop->first)
-                @php
-                  $skipDays = $this->startWeekOnSunday ? 7 : 6;
-                @endphp
-                @for ($i = 0; $i < ($day->dayOfWeek === 0 ? $skipDays : ($day->dayOfWeek - ($this->startWeekOnSunday ? 0 : 1))); $i++)
-                  <div></div>
-                @endfor
-              @endif
-              <div wire:key="{{$dayData->date->format('ymd')}}"
-                class="@if ($this->type === self::TYPE_RANGE_SINGLE) !rounded-l-full !rounded-r-full @endif @if ($this->isStartRange($day)) is-start-date 
-                @elseif ($this->isEndRange($day)) is-end-date
-                @elseif ($this->isInRange($day)) is-today @endif @if (!$dayData->disabled && !$this->isDisabled($day)) cursor-pointer
-                @else
-                  cursor-not-allowed
-                  opacity-25 
-                @endif 
-                day-item"
-                @if ($dayData->toolTip) x-tooltip.raw.html="{{ $dayData->toolTip }}" @endif
-                @if (!$dayData->disabled && !$this->isDisabled($day)) wire:click="triggerDate('{{ $day->format(config('livewire-datepicker.event_date_format')) }}')" @endif>
-                <div
-                  class="@if ($this->isSelected($day)) border-2 !border-primary-900 text-white shadow-lg
-                  @elseif ($day->isToday()) font-bold @endif {{ $dayData->classes ?? '' }} mx-auto flex h-7 w-7 items-center justify-center rounded-full">
-                  {{ $day->format('j') }}
+        <div class="container__months">
+            <div class="month-item">
+                <div class="month-item-header">
+                    <button type="button" class="button-previous-month" wire:click="goPreviousMonth">
+                        @lang('Previous month')
+                    </button>
+                    <div>
+                        <strong class="month-item-name">{{ $monthName }}</strong>
+                        <span class="month-item-year">{{ $year }}</span>
+                    </div>
+                    <button type="button" class="button-next-month" wire:click="goNextMonth">
+                        @lang('Next month')</span>
+                    </button>
                 </div>
-              </div>
-            @endforeach
-          </div>
+                <div class="month-item-weekdays-row">
+                    @foreach ($weekDays as $index => $name)
+                        <div title="{{ $name }}">
+                            {{ \Illuminate\Support\Str::substr($name, 0, 1) }}
+                        </div>
+                    @endforeach
+                </div>
+                <div class="container__days">
+                    @foreach ($dates as $index => $day)
+                        @php
+                            /** @var \Haringsrob\LivewireDatepicker\Dto\DatePickerDayData $dayData */
+                            $dayData = $this->getAvailabilityFor($day);
+                        @endphp
+                        @if ($loop->first)
+                            @php
+                                $skipDays = $this->startWeekOnSunday ? 7 : 6;
+                            @endphp
+                            @for ($i = 0; $i < ($day->dayOfWeek === 0 ? $skipDays : $day->dayOfWeek - ($this->startWeekOnSunday ? 0 : 1)); $i++)
+                                <div></div>
+                            @endfor
+                        @endif
+                        <div wire:key="{{ $dayData->date->format('ymd') }}"
+                            class="@if ($this->type === self::TYPE_RANGE_SINGLE) !rounded-l-full !rounded-r-full @endif @if ($this->isStartRange($day)) is-start-date 
+              @elseif ($this->isEndRange($day)) is-end-date
+              @elseif ($this->isInRange($day)) is-today @endif @if (!$dayData->disabled && !$this->isDisabled($day)) cursor-pointer
+              @else
+                cursor-not-allowed
+                opacity-25 @endif 
+              day-item"
+                            @if ($dayData->toolTip) x-tooltip.raw.html="{{ $dayData->toolTip }}" @endif
+                            @if (!$dayData->disabled && !$this->isDisabled($day)) wire:click="triggerDate('{{ $day->format(config('livewire-datepicker.event_date_format')) }}')" @endif>
+                            {{-- <div
+                                class="@if ($this->isSelected($day)) border-2 !border-primary-900 text-white shadow-lg
+                @elseif ($day->isToday()) font-bold @endif {{ $dayData->classes ?? '' }} mx-auto flex h-7 w-7 items-center justify-center rounded-full">
+                                {{ $day->format('j') }}
+                            </div> --}}
+                        </div>
+                    @endforeach
+                </div>
+            </div>
         </div>
-      </div>
     </div>
     <div class="container__tooltip"></div>
-  </div>
 </div>
